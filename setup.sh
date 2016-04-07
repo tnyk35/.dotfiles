795fa48f63db3b4c04e33be8be5bf7e5bc52e008
Yuki Tani <yuki@Yuki-no-Mac-mini.local>
2016-04-08 02:18:19 +0900

    oh-my-zshパス指定修正

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ git submodule init
 git submodule update
 
 # シンボリックリンクの設定
-DOT_FILES=(.vimrc .oh-my-zsh .tmux.conf .zshrc)
+DOT_FILES=(.vimrc .oh-my-zsh .tmux.conf .zshrc oh-my-zsh)
 
 for file in ${DOT_FILES[@]}
 do
